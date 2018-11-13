2368ddd3bf25272505b8cd7c27406e2f5ad42b86
Cara Hill <cara@rabidtech.co.nz>
2018-11-13 16:21:31 +1300

    Refactored.

--- bob.js
+++ bob.js
@@ -1,13 +1,30 @@
 export const hey = (message) => {
-  if(message.includes(".")) {
-    return "Whatever.";
-  }
+  let answers = ["Whatever.", "Whoa, chill out!", "Sure.", "Calm down, I know what I'm doing!", "Fine. Be that way!"];
 
-  if(message == message.toUpperCase()) {
-    return "Whoa, chill out!";
+  switch(sayingResult(message)) {
+    case 0:
+      return answers[0];
+    case 1:
+      return answers[1];
+    case 2:
+      return answers[2];
+    case 3:
+      return answers[3];
+    case 4:
+      return ansers[4];
   }
+};
 
-  if(message.includes("?")) {
-    return "Sure.";
+let sayingResult = (message) => {
+  if(message == message.toUpperCase() && message.includes("?")) {
+    return 3;
+  } else if(message == message.toUpperCase()) {
+    return 1;
+  } else if (message.includes("?") && message[message.length -1] == "?") {
+    return 2;
+  } else if (message == "") {
+    return 4;
+  } else {
+    return 0;
   }
 };
