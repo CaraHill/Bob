cb912217aba3489d8cf5119a8e1e9a93af048ed8
Cara Hill <cara@rabidtech.co.nz>
2018-11-13 17:05:46 +1300

    Refactored

--- bob.js
+++ bob.js
@@ -18,15 +18,31 @@ export const hey = (message) => {
 };
 
 let sayingResult = (message) => {
-  if(message == message.toUpperCase() && message.includes("?") && /[a-zA-Z]/.test(message)) {
+  if(checkForInsolence(message)) {
     return 3;
-  } else if(message == message.toUpperCase() && /[a-zA-Z]/.test(message)) {
+  } else if(checkForDefensiveness(message)) {
     return 1;
-  } else if (message.includes("?") && message[message.length -1] == "?") {
+  } else if (checkForAgreement(message)) {
     return 2;
-  } else if (message == "" || (message.includes("  ") && !/[\S]+/.test(message)) || message.includes("\t")) {
+  } else if (checkForAnger(message)) {
     return 4;
   } else {
     return 0;
   }
+};
+
+let checkForInsolence = (message) => {
+  return message == message.toUpperCase() && message.includes("?") && /[a-zA-Z]/.test(message);
+};
+
+let checkForDefensiveness = (message) => {
+  return message == message.toUpperCase() && /[a-zA-Z]/.test(message);
+};
+
+let checkForAgreement = (message) => {
+  return message.includes("?") && message[message.length -1] == "?";
+};
+
+let checkForAnger = (message) => {
+  return message == "" || (message.includes("  ") && !/[\S]+/.test(message)) || message.includes("\t");
 };
